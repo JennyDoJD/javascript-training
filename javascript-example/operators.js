f3eb29b7f0a53f0d7afe9242ea48d0e329e4fc7c
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-03-22 15:55:12 +0700

    feat: do example of an operators

---
+++ javascript-example/operators.js
@@ -0,0 +1,35 @@
+let x = 10;
+
+// the addition operator (+)
+const addition = x + 2;
+
+// the subtraction operator (-)
+const subtraction = x - 3;
+
+// the division operator (/)
+const division = x / 2;
+
+// the remainder operator (%)
+const remainder = x % 4;
+
+// the multiplication operator (*)
+const multiplication = x * 5;
+
+// the exponentiation operator (**)
+const exponentiation = x ** 2;
+
+
+console.log(
+  addition +
+    "\n" +
+    subtraction +
+    "\n" +
+    division +
+    "\n" +
+    remainder +
+    "\n" +
+    multiplication +
+    "\n" +
+    exponentiation +
+    "\n"
+);
