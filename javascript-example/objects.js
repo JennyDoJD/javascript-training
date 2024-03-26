fae5e6779e5ece0f2dea79debb4f82eb8f9d6ff3
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-03-26 15:33:29 +0700

    feat: update example  of objects

--- javascript-example/objects.js
+++ javascript-example/objects.js
@@ -12,7 +12,7 @@ const student = {
 const myKey = 'id';
 
 console.log(student.name); // Output: Van B
-console.log(student.avg mark); // SyntaxError
+//console.log(student.avg mark); // SyntaxError
 console.log(student["avg mark"]); // Output: 9
 console.log(student[myKey]); // Output: 1
 console.log(student.getName()); // Output: Van B
@@ -30,3 +30,29 @@ console.log(student.mark); // Output: 10
 // delete key
 delete student.name;
 console.log(student.name); // Output: undefined
+
+// date object
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+console.log(`${day}/${month}/${year}`);
+
+// different arrow functions and regular functions in objects
+const car = {
+  brand: 'Ford',
+  model: 'Fiesta',
+  start: () => {
+    console.log(`Started ${this.brand} ${this.model}`); // not going to work
+  }
+};
+car.start(); // Output: Started undefined undefined
+
+const car1 = {
+  brand: 'Ford',
+  model: 'Fiesta',
+  goTo: function(destination) {
+    console.log(`Going to ${destination}`);
+  }
+};
+car1.goTo('Rome'); // Output: Going to Rome
