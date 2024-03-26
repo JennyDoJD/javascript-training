8b5fe443ce50eab567a4d3b14ad61bad456663d4
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-03-26 14:29:43 +0700

    feat: do example of delete the object's key

--- javascript-example/objects.js
+++ javascript-example/objects.js
@@ -5,7 +5,6 @@ const student = {
   isHero: true,
   'avg mark': 9,
 }
-
 console.log(student.name); // Output: Van B
 console.log(student.avg mark); // SyntaxError
 console.log(student["avg mark"]); // Output: 9
@@ -17,6 +16,9 @@ console.log(student[key]); // Output: 9
 // set new key to object
 student.age = 18;
 student['mark'] = 10;
-
 console.log(student.age); // Output: 18
 console.log(student.mark); // Output: 10
+
+// delete key
+delete student.name;
+console.log(student.name); // Output: undefined
