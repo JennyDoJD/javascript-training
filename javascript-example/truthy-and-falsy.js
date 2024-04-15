ba84847664bc5325f421403302c64412e6802b0f
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-04-15 17:20:11 +0700

    feat: do example of || and ?? operator

--- javascript-example/truthy-and-falsy.js
+++ javascript-example/truthy-and-falsy.js
@@ -14,3 +14,13 @@ if (salary) {
 }
 
 // Output: Salary is undefined
+
+// || and ??
+console.log(1 || 0); // Output: 1 (the first truthy value is 1)
+console.log(null || 2); // Output: 2 (the first truthy value is 2)
+console.log("" || undefined || 0 || 10); // Output: 10 (the first truthy value is 10)
+console.log(null || 100 || 5 || undefined); // Output: 100 (the first truthy value is 100)
+console.log("" || 0 || null); // Output: null (no truthy value, return the final value)
+
+console.log(0 ?? 1); // Output: 0 (0 is not null or undefined)
+console.log(null ?? false ?? undefined); // Output: false (false value is the first value that is not null or undefined)
