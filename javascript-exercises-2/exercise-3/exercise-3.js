9982716ea983f03287397c84ecbefa4c23881097
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-04-16 14:36:42 +0700

    refactor: add comment for exercise

--- javascript-exercises-2/exercise-3/exercise-3.js
+++ javascript-exercises-2/exercise-3/exercise-3.js
@@ -1,3 +1,6 @@
+// Write a JavaScript program to set paragraph background color.
+
+
 function changeBackground() {
   let title = document.getElementById('title');
   title.style.backgroundColor = 'grey';
