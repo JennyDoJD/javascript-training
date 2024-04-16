4e9e5d9777e9e968d8c12b383cfdd57600b2dbf6
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-04-16 09:43:26 +0700

    feat: do exercise 1 of javascript DOM

---
+++ javascript-exercises-2/exercise-1/exercise-1.js
@@ -0,0 +1,11 @@
+// Clicking on the button the font, font size, and color of the paragraph text will be changed.
+
+function js_style() {
+  // get the paragraph element by its id
+  let paragraph = document.getElementById('text');
+
+  // modify the style properties
+  paragraph.style.font = 'Arial, sans-serif';
+  paragraph.style.fontSize = '20px';
+  paragraph.style.color = 'pink';
+}
