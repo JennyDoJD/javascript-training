aab27f18ff14a5c738b1987c3ae7839893283c09
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-04-16 17:16:08 +0700

    feat: do exercise 10 of javascript DOM

---
+++ javascript-exercises-2/exercise-10/exercise-10.js
@@ -0,0 +1,10 @@
+//  Write a JavaScript program to calculate sphere volume.
+
+function calculateVolume() {
+  // get the radius value from the input
+  const radius = parseFloat(document.getElementById('radius').value);
+  // calculate the volume of the sphere
+  const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  // display the values
+  document.getElementById('volume').value = volume.toFixed(4);
+}
