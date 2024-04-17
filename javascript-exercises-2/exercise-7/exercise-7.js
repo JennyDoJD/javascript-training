9fc614e4f74a0519942ea0ef305910b188fdacef
JennyDoJD <loido.ued@gmail.com>
2024-04-17 07:36:23 +0700

    feat: do exercise 7 of javascript DOM

---
+++ javascript-exercises-2/exercise-7/exercise-7.js
@@ -0,0 +1,17 @@
+function createTable() {
+  let rows = parseInt(prompt('Enter the number of rows:'));
+  let cols = parseInt(prompt('Enter the number of columns:'));
+
+  const table = document.getElementById('myTable');
+    // Clear existing table content
+    table.innerHTML = '';
+
+    // Create rows and cells
+  for (let i = 0; i < rows; i++) {
+    const row = table.insertRow(i);
+      for (let j = 0; j < cols; j++) {
+        const cell = row.insertCell(j);
+        cell.innerHTML = 'Row-' + i + ' Column-' + j;
+      }
+    }
+}
