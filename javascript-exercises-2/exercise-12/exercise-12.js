46505ae7beb03391dea6a323e659450ad9d98c4d
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-04-17 09:50:56 +0700

    feat: do exercise 12 of javascript DOM

---
+++ javascript-exercises-2/exercise-12/exercise-12.js
@@ -0,0 +1,22 @@
+// Write a JavaScript program to highlight the bold words of the following paragraph, on mouse over a certain link.
+
+
+function highlightText() {
+  // get all the strong elements within the document
+  const boldText = document.querySelectorAll('strong');
+
+  // add the 'bold-color' class to each strong element
+  boldText.forEach(text => {
+    text.classList.add('bold-color');
+  });
+}
+
+function returnNormalText() {
+  // get all the strong element withing the document
+  const boldText = document.querySelectorAll('strong');
+
+  // remove the 'bold-color' class to each strong element
+  boldText.forEach(text => {
+    text.classList.remove('bold-color');
+  });
+}
