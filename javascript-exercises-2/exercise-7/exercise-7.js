a217b72332829766bbf74e5c0b7f26f7742af768
JennyDoJD <loido.ued@gmail.com>
2024-04-17 07:43:16 +0700

    style: format code according to airbnb javascript style guide()

--- javascript-exercises-2/exercise-7/exercise-7.js
+++ javascript-exercises-2/exercise-7/exercise-7.js
@@ -3,15 +3,14 @@ function createTable() {
   let cols = parseInt(prompt('Enter the number of columns:'));
 
   const table = document.getElementById('myTable');
-    // Clear existing table content
-    table.innerHTML = '';
-
-    // Create rows and cells
+  // clear existing table content
+  table.innerHTML = '';
+  // create rows and cells
   for (let i = 0; i < rows; i++) {
     const row = table.insertRow(i);
-      for (let j = 0; j < cols; j++) {
-        const cell = row.insertCell(j);
-        cell.innerHTML = 'Row-' + i + ' Column-' + j;
-      }
+    for (let j = 0; j < cols; j++) {
+      const cell = row.insertCell(j);
+      cell.innerHTML = 'Row-' + i + ' Column-' + j;
     }
+  }
 }
