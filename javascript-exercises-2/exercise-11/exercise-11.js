601fb6948baa4624689eb1abbb3ee9000089929e
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-04-17 09:27:47 +0700

    feat: do exercise 11 of javascript DOM

---
+++ javascript-exercises-2/exercise-11/exercise-11.js
@@ -0,0 +1,36 @@
+// Write a JavaScript program to display a random image (clicking on a button) from the following list.
+
+
+function displayImage() {
+  const images = [
+    {
+      url: 'http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg',
+      width: '240',
+      height: '160',
+    },
+    {
+      url: 'http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg',
+      width: '320',
+      height: '195',
+    },
+    {
+      url: 'http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg',
+      width: '500',
+      height: '343'
+    }
+  ];
+
+  // get a random index within the range of images array
+  const randomIndex = Math.floor(Math.random() * images.length);
+
+  // get the random image object from the array
+  const randomImage = images[randomIndex];
+
+  // get the <img> element
+  const imgElement = document.getElementById('randomImage');
+
+  // set the src attribute of the <img> element to the random image URL
+  imgElement.src = randomImage.url;
+  imgElement.width = randomImage.width;
+  imgElement.height = randomImage.height;
+}
