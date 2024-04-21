21a86a89ce5292119c3a96c8625d2896753026d4
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:41:34 +0700

    style: capitalize first character of comments

--- javascript-exercises-2/exercise-12/exercise-12.js
+++ javascript-exercises-2/exercise-12/exercise-12.js
@@ -1,19 +1,19 @@
 // Write a JavaScript program to highlight the bold words of the following paragraph, on mouse over a certain link.
 function highlightText() {
-  // get all the strong elements within the document
+  // Get all the strong elements within the document
   const boldText = document.querySelectorAll('strong');
 
-  // add the 'bold-color' class to each strong element
+  // Add the 'bold-color' class to each strong element
   boldText.forEach(text => {
     text.classList.add('bold-color');
   });
 }
 
 function returnNormalText() {
-  // get all the strong element withing the document
+  // Get all the strong element withing the document
   const boldText = document.querySelectorAll('strong');
 
-  // remove the 'bold-color' class to each strong element
+  // Remove the 'bold-color' class to each strong element
   boldText.forEach(text => {
     text.classList.remove('bold-color');
   });
