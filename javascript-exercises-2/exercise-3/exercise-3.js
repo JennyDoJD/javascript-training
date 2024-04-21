5f611b64c45ea434f0ec1f43f06208d21612634f
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:06:16 +0700

    style: remove blank line

--- javascript-exercises-2/exercise-3/exercise-3.js
+++ javascript-exercises-2/exercise-3/exercise-3.js
@@ -1,6 +1,4 @@
 // Write a JavaScript program to set paragraph background color.
-
-
 function changeBackground() {
   let title = document.getElementById('title');
   title.style.backgroundColor = 'grey';
