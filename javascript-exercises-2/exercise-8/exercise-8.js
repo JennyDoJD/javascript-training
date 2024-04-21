f9ec7948564a2be771038c356b452e5099f1b810
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:20:07 +0700

    fix: change let variable to const variable

--- javascript-exercises-2/exercise-8/exercise-8.js
+++ javascript-exercises-2/exercise-8/exercise-8.js
@@ -1,5 +1,5 @@
 // Write a JavaScript program to remove items from a drop-down list.
 function removeColor() {
-  let selectElement = document.getElementById('colorSelect');
+  const selectElement = document.getElementById('colorSelect');
   selectElement.remove(selectElement.selectedIndex);
 }
