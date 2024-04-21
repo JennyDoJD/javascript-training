e0f2b1b2f0eb0559cd8a2b8b8c07c33ff1cedbe9
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:05:37 +0700

    style: rename id attributes

--- javascript-exercises-2/exercise-2/exercise-2.js
+++ javascript-exercises-2/exercise-2/exercise-2.js
@@ -1,7 +1,7 @@
 // Write a JavaScript function to get the values of First and Last names of the following form.
 function getFormvalue() {
   // Get the form element
-  let form = document.getElementById('form1');
+  let form = document.getElementById('user-profile');
 
   // Get the values of the first and last name inputs
   let firstName = form.elements['fname'].value;
