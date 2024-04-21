c71a77ae6cd1fa2667446b5a671aaa1e8179b6b0
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:21:21 +0700

    style: rename id attributes

--- javascript-exercises-2/exercise-8/exercise-8.js
+++ javascript-exercises-2/exercise-8/exercise-8.js
@@ -1,5 +1,5 @@
 // Write a JavaScript program to remove items from a drop-down list.
 function removeColor() {
-  const selectElement = document.getElementById('colorSelect');
+  const selectElement = document.getElementById('select-color');
   selectElement.remove(selectElement.selectedIndex);
 }
