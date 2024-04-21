c6836021a8a5320b5c97cd11e701770d921f88bd
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:25:02 +0700

    style: rename id attributes

--- javascript-exercises-2/exercise-9/exercise-9.js
+++ javascript-exercises-2/exercise-9/exercise-9.js
@@ -1,6 +1,6 @@
 // Write a JavaScript program to count and display dropdown list items in an alert window.
 function getOptions() {
-  const selectElement = document.getElementById('mySelect');
+  const selectElement = document.getElementById('select-color');
   const options = selectElement.options;
   const optionCount = options.length;
   let optionList = '';
