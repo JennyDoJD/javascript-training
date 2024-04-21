6b7480205d812b8c9dd914b23b6f86238477fc75
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:45:41 +0700

    fix: change let variable to const variable

--- javascript-exercises-2/exercise-2/exercise-2.js
+++ javascript-exercises-2/exercise-2/exercise-2.js
@@ -1,11 +1,11 @@
 // Write a JavaScript function to get the values of First and Last names of the following form.
 function getFormvalue() {
   // Get the form element
-  let form = document.getElementById('user-profile');
+  const form = document.getElementById('user-profile');
 
   // Get the values of the first and last name inputs
-  let firstName = form.elements['fname'].value;
-  let lastName = form.elements['lname'].value;
+  const firstName = form.elements['fname'].value;
+  const lastName = form.elements['lname'].value;
 
   // Display the values
   alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
