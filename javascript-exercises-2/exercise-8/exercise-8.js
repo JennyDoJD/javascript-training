f2e6299e83ac0efe5fde807030f8a2183a11a3c2
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:19:43 +0700

    style: remove blank line

--- javascript-exercises-2/exercise-8/exercise-8.js
+++ javascript-exercises-2/exercise-8/exercise-8.js
@@ -1,6 +1,4 @@
 // Write a JavaScript program to remove items from a drop-down list.
-
-
 function removeColor() {
   let selectElement = document.getElementById('colorSelect');
   selectElement.remove(selectElement.selectedIndex);
