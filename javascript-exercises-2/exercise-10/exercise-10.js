028a0ebe6b9c3935a5f1cb7fd1abf2485e50295b
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:29:10 +0700

    fix: change let variable to const variable

--- javascript-exercises-2/exercise-10/exercise-10.js
+++ javascript-exercises-2/exercise-10/exercise-10.js
@@ -1,9 +1,9 @@
 //  Write a JavaScript program to calculate sphere volume.
 function calculateVolume() {
-  // get the radius value from the input
+  // Get the radius value from the input
   const radius = parseFloat(document.getElementById('get-radius').value);
-  // calculate the volume of the sphere
+  // Calculate the volume of the sphere
   const volume = (4/3) * Math.PI * Math.pow(radius, 3);
-  // display the values
+  // Display the values
   document.getElementById('get-volume').value = volume.toFixed(4);
 }
