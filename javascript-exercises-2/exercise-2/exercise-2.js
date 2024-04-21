8db6a10b6a498d72980e78baa1a671aead78d94f
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:03:26 +0700

    style: capitalize first character of comments

--- javascript-exercises-2/exercise-2/exercise-2.js
+++ javascript-exercises-2/exercise-2/exercise-2.js
@@ -1,12 +1,12 @@
 // Write a JavaScript function to get the values of First and Last names of the following form.
 function getFormvalue() {
-  // get the form element
+  // Get the form element
   let form = document.getElementById('form1');
 
-  // get the values of the first and last name inputs
+  // Get the values of the first and last name inputs
   let firstName = form.elements['fname'].value;
   let lastName = form.elements['lname'].value;
 
-  // display the values
+  // Display the values
   alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
 }
