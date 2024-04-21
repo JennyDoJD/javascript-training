f72ccc2fa279f334b3adca4dac8340ba8bc38569
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:24:09 +0700

    fix: change let variable to const variable

--- javascript-exercises-2/exercise-9/exercise-9.js
+++ javascript-exercises-2/exercise-9/exercise-9.js
@@ -1,8 +1,8 @@
 // Write a JavaScript program to count and display dropdown list items in an alert window.
 function getOptions() {
-  let selectElement = document.getElementById('mySelect');
-  let options = selectElement.options;
-  let optionCount = options.length;
+  const selectElement = document.getElementById('mySelect');
+  const options = selectElement.options;
+  const optionCount = options.length;
   let optionList = '';
 
   for (let i = 0; i < optionCount; i++) {
