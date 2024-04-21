4099c498b13db69a4ede46c85e5f157fba8b71f5
JennyDoJD <loido.ued@gmail.com>
2024-04-21 21:53:34 +0700

    style: capitalize first character of comments

--- javascript-exercises-2/exercise-1/exercise-1.js
+++ javascript-exercises-2/exercise-1/exercise-1.js
@@ -1,9 +1,9 @@
 // Clicking on the button the font, font size, and color of the paragraph text will be changed.
 function changeText() {
-  // get the paragraph element by its id
+  // Get the paragraph element by its id
   const paragraph = document.getElementById('text');
 
-  // modify the style properties
+  // Modify the style properties
   paragraph.style.font = 'Arial, sans-serif';
   paragraph.style.fontSize = '20px';
   paragraph.style.color = 'pink';
