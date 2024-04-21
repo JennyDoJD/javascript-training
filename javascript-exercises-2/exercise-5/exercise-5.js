bbc01fc489adfb11ae72ae8636af5755d4819ebb
JennyDoJD <loido.ued@gmail.com>
2024-04-21 22:12:28 +0700

    style: capitalize first character of comments

--- javascript-exercises-2/exercise-5/exercise-5.js
+++ javascript-exercises-2/exercise-5/exercise-5.js
@@ -1,21 +1,21 @@
 // Write a JavaScript function to add rows to a table.
 function insertRow() {
-  // get a reference to the table
+  // Get a reference to the table
   const table = document.getElementById('sampleTable');
 
-  // if the table doesn't exist, create a new table and append it to the document
+  // If the table doesn't exist, create a new table and append it to the document
   if (!table) {
     table = document.createElement('table');
     table.id = 'sampleTable';
     document.body.appendChild(table);
   }
 
-  // create a new row and two data cells
+  // Create a new row and two data cells
   const newRow = table.insertRow();
   const cell1 = newRow.insertCell(0);
   const cell2 = newRow.insertCell(1);
 
-  // set content for the data cells
+  // Set content for the data cells
   cell1.innerHTML = 'New row cell1';
   cell2.innerHTML = 'New row cell2';
 };
