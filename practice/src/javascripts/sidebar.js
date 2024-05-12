54c191c9fafa6574cbcd1ceb2b3c7920815aa8e3
Loi Do <110828723+JennyDoJD@users.noreply.github.com>
2024-05-13 05:27:32 +0700

    style: change single quote into double quotes

--- practice/src/javascripts/sidebar.js
+++ practice/src/javascripts/sidebar.js
@@ -1,9 +1,9 @@
 // Identify the links in the sidebar and add/remove the 'active' class when clicked
-let list = document.querySelectorAll(".nav-item");
+let list = document.querySelectorAll('.nav-item');
 
 function activeLink() {
-  list.forEach((item) => item.classList.remove("active"));
-  this.classList.add("active");
+  list.forEach((item) => item.classList.remove('active'));
+  this.classList.add('active');
 }
 
-list.forEach((item) => item.addEventListener("click", activeLink));
+list.forEach((item) => item.addEventListener('click', activeLink));
