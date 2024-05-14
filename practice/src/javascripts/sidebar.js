89f78a2c66577fe80cf444e83f63f6067bac099e
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-14 11:05:13 +0700

    fix: apply event delegation for sidebar

--- practice/src/javascripts/sidebar.js
+++ practice/src/javascripts/sidebar.js
@@ -1,9 +1,13 @@
 // Identify the links in the sidebar and add/remove the 'active' class when clicked
-let list = document.querySelectorAll('.nav-item');
+document.querySelector('.nav-list').addEventListener('click', function (event) {
+  // Check if the clicked element is a nav-link inside a nav-item
+  if (event.target.classList.contains('nav-link')) {
+    // Remove 'active' class from all nav-items
+    document
+      .querySelectorAll('.nav-item')
+      .forEach((item) => item.classList.remove('active'));
 
-function activeLink() {
-  list.forEach((item) => item.classList.remove('active'));
-  this.classList.add('active');
-}
-
-list.forEach((item) => item.addEventListener('click', activeLink));
+    // Add 'active' class to the parent nav-item of the clicked nav-link
+    event.target.closest('.nav-item').classList.add('active');
+  }
+});
