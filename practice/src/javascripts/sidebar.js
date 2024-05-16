e8e9ae4cc657c8f23e536de98f8fe31bebabd609
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-16 11:29:37 +0700

    fix: improved to that only one item is active at a time

--- practice/src/javascripts/sidebar.js
+++ practice/src/javascripts/sidebar.js
@@ -2,12 +2,17 @@
 document.querySelector('.nav-list').addEventListener('click', function (event) {
   // Check if the clicked element is a nav-link inside a nav-item
   if (event.target.classList.contains('nav-link')) {
-    // Remove 'active' class from all nav-items
-    document
-      .querySelectorAll('.nav-item.active')
-      .forEach((item) => item.classList.remove('active'));
+    const clickedNavItem = event.target.closest('.nav-item');
 
-    // Add 'active' class to the parent nav-item of the clicked nav-link
-    event.target.closest('.nav-item').classList.add('active');
+    // Check if the clicked nav-item is already active
+    if (!clickedNavItem.classList.contains('active')) {
+      // Remove 'active' class from all nav-items
+      document
+        .querySelectorAll('.nav-item.active')
+        .forEach((item) => item.classList.remove('active'));
+
+      // Add 'active' class to the parent nav-item of the clicked nav-link
+      clickedNavItem.classList.add('active');
+    }
   }
 });
