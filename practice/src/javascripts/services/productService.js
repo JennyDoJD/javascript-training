9165ff003c0303bdbee52889e4845f32d39d8ef0
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-24 17:02:08 +0700

    fix: create product service extend base service

--- practice/src/javascripts/services/productService.js
+++ practice/src/javascripts/services/productService.js
@@ -1,15 +1,8 @@
+import BaseService from './baseService';
 import { API } from '../constants/apiUrl';
 
-// Function to fetch products from the API
-export async function fetchProducts() {
-  try {
-    const response = await fetch(`${API.BASE_URL}/${API.PRODUCTS_ENDPOINT}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch products');
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
+export default class ProductService extends BaseService {
+  constructor() {
+    super(API.PRODUCTS_ENDPOINT);
   }
 }
