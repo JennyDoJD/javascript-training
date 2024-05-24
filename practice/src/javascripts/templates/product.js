976dbbbffd7cf477c1a35f7d09534750563e2395
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-24 10:52:28 +0700

    fix: use arrow function to render product cards

--- practice/src/javascripts/templates/product.js
+++ practice/src/javascripts/templates/product.js
@@ -1,35 +1,33 @@
 import iconAction from '../../assets/images/icons/icons.svg';
 
-// Function to render product cards
-export function ProductCard(product) {
-  return `
-    <div class="card card-product">
-      <div class="card-header">
-        <svg width="15" height="15">
-          <use xlink:href="${iconAction}#delete-icon" />
-        </svg>
-      </div>
-      <div>
-        <figure class="card-image">
-          <img src="${product.imageURL}" alt="food-image" class="card-item" />
-        </figure>
-        <div class="card-text">
-          <p class="tertiary-title card-name">${product.name}</p>
-          <div class="main-desc card-desc">
-            <span>$ ${product.price}</span>
-            <span class="circle">&#8729;</span>
-            <span>${product.quantity} Bowls</span>
-          </div>
+// Arrow function to render product cards
+export const ProductCard = (product) => `
+  <div class="card card-product">
+    <div class="card-header">
+      <svg width="15" height="15">
+        <use xlink:href="${iconAction}#delete-icon" />
+      </svg>
+    </div>
+    <div>
+      <figure class="card-image">
+        <img src="${product.imageURL}" alt="food-image" class="card-item" />
+      </figure>
+      <div class="card-text">
+        <p class="tertiary-title card-name">${product.name}</p>
+        <div class="main-desc card-desc">
+          <span>$ ${product.price}</span>
+          <span class="circle">&#8729;</span>
+          <span>${product.quantity} Bowls</span>
         </div>
       </div>
-      <div>
-        <a href="javascript:void(0)" class="btn btn-edit">
-          <svg width="20" height="20">
-            <use xlink:href="${iconAction}#edit-icon" />
-          </svg>
-          Edit dish
-        </a>
-      </div>
     </div>
-  `;
-}
+    <div>
+      <a href="javascript:void(0)" class="btn btn-edit">
+        <svg width="20" height="20">
+          <use xlink:href="${iconAction}#edit-icon" />
+        </svg>
+        Edit dish
+      </a>
+    </div>
+  </div>
+`;
