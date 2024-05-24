98689ab61fa23277e86d0a2df67e09f8915cb184
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-24 17:08:59 +0700

    fix:  fetches data from the API using the specified endpoint

---
+++ practice/src/javascripts/helpers/api.js
@@ -0,0 +1,22 @@
+import { API } from '../constants/apiUrl';
+
+const APIHelper = {
+  /*
+   * Fetches data from the API using the specified endpoint.
+   * It defines an asynchronous function 'get' to fetch data from the API.
+   */
+  async get(endpoint) {
+    try {
+      const response = await fetch(`${API.BASE_URL}/${endpoint}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch products');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+export { APIHelper };
