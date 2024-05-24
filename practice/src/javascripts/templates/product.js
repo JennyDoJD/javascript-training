581d3c5595fb40fffc789b1c4a199dc9b25c6acf
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-24 16:58:07 +0700

    fix: create classes to managing the display of product cards

--- practice/src/javascripts/templates/product.js
+++ practice/src/javascripts/templates/product.js
@@ -1,33 +1,80 @@
+/**
+ * This module provides a ProductListView class for managing the display of product cards.
+ * It contains methods for creating product cards, clearing the main container, and rendering products.
+ */
 import iconAction from '../../assets/images/icons/icons.svg';
 
-// Arrow function to render product cards
-export const ProductCard = (product) => `
-  <div class="card card-product">
-    <div class="card-header">
-      <svg width="15" height="15">
-        <use xlink:href="${iconAction}#delete-icon" />
-      </svg>
-    </div>
-    <div>
-      <figure class="card-image">
-        <img src="${product.imageURL}" alt="food-image" class="card-item" />
-      </figure>
-      <div class="card-text">
-        <p class="tertiary-title card-name">${product.name}</p>
-        <div class="main-desc card-desc">
-          <span>$ ${product.price}</span>
-          <span class="circle">&#8729;</span>
-          <span>${product.quantity} Bowls</span>
+export default class ProductListView {
+  /**
+   * Create HTML markup for a product card
+   */
+  static createProductCard(product) {
+    const { id, name, price, imageURL, quantity } = product;
+
+    return `
+    <div class="card card-product">
+        <div class="card-header">
+          <svg width="15" height="15" data-productId=${id}>
+            <use xlink:href="${iconAction}#delete-icon" />
+          </svg>
         </div>
-      </div>
-    </div>
-    <div>
-      <a href="javascript:void(0)" class="btn btn-edit">
-        <svg width="20" height="20">
-          <use xlink:href="${iconAction}#edit-icon" />
-        </svg>
-        Edit dish
-      </a>
-    </div>
-  </div>
-`;
+        <div>
+          <figure class="card-image">
+            <img src="${imageURL}" alt="food-image" class="card-item" />
+          </figure>
+          <div class="card-text">
+            <p class="tertiary-title card-name">${name}</p>
+            <div class="main-desc card-desc">
+              <span>$ ${price}</span>
+              <span class="circle">&#8729;</span>
+              <span>${quantity} Bowls</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <a href="javascript:void(0)" class="btn btn-edit">
+            <svg width="20" height="20">
+              <use xlink:href="${iconAction}#edit-icon" />
+            </svg>
+            Edit dish
+          </a>
+        </div>
+        </div>
+    `;
+  }
+
+  /**
+   * Clear the main container
+   */
+  static clearMainContainer() {
+    const mainContent = document.getElementById('product-list');
+    mainContent.innerHTML = '';
+  }
+
+  /**
+   * Render products
+   */
+
+  static renderProducts(products) {
+    this.clearMainContainer();
+
+    const mainContent = document.getElementById('product-list');
+
+    if (!Array.isArray(products)) {
+      throw new Error('Invalid data received from API');
+    }
+
+    if (products.length === 0) {
+      mainContent.innerHTML = `<h2 class="main-title">No Result</h2>`;
+      return;
+    }
+
+    let productListHTML = '<div class="card-product-list">';
+    products.forEach((product) => {
+      productListHTML += this.createProductCard(product);
+    });
+    productListHTML += '</div>';
+
+    mainContent.innerHTML = productListHTML;
+  }
+}
