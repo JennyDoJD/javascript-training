6d5331033aea53083fe60b38d6ad99720819e486
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-28 15:08:36 +0700

    fix: change comment

--- practice/src/javascripts/products/productList.js
+++ practice/src/javascripts/products/productList.js
@@ -9,12 +9,12 @@ export default class ProductList {
 
   /**
    * Fetch all products from the ProductService
-   * Render the products using the ProductListView component
+   * Render the products using the ProductTemplate component
    * Log an error message if rendering fails
    */
   async renderProducts() {
     try {
-      const products = await this.productService.getAllProducts();
+      const products = await this.productService.getAll();
       ProductTemplate.renderProducts(products);
     } catch (error) {
       console.error('Failed to render products:', error);
