f3f9594b25fe795e02383d4eb1f892d34b2e2b7c
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-29 09:36:07 +0700

    feat: add constant MESSAGES for item properties

---
+++ practice/src/javascripts/constants/message.js
@@ -0,0 +1,11 @@
+const MESSAGES = {
+  ADD_PRODUCT_SUCCESS_MSG: 'The dish was added successfully!',
+  ADD_PRODUCT_FAILED_MSG: 'The dish was added unsuccessfully!',
+  DELETE_PRODUCT_SUCCESS_MSG: 'The dish was deleted successfully!',
+  DELETE_PRODUCT_FAILED_MSG: 'The dish was deleted successfully!',
+  EDIT_PRODUCT_SUCCESS_MSG: 'The dish was edited successfully!',
+  EDIT_PRODUCT_SUCCESS_MSG: 'The dish was edited unsuccessfully!',
+  SEARCH_PRODUCT_LIST_EMPTY_HEADING: 'No dishes found!',
+};
+
+export { MESSAGES };
