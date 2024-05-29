ea21410016e9bbaa436e10a72818b8201a33cbae
Loi Do <110828723+JennyDoJD@users.noreply.github.com>
2024-05-29 08:53:41 +0700

    fix: remove try catch

--- practice/src/javascripts/products/productList.js
+++ practice/src/javascripts/products/productList.js
@@ -11,15 +11,9 @@ export default class ProductList {
   /**
    * Fetch all products from the ProductService
    * Render the products using the ProductTemplate component
-   * Log an error message if rendering fails
    */
   async renderProducts() {
-    try {
-      const products = await this.productService.getAll();
-      this.productTemplate.renderProducts(products);
-      
-    } catch (error) {
-      console.error('Failed to render products:', error);
-    }
+    const products = await this.productService.getAll();
+    this.productTemplate.renderProducts(products);
   }
 }
