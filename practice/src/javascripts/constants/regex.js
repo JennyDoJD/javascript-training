45a867feb796f0a17bc30118a2ae627d86198dbf
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-29 09:36:28 +0700

    feat: add constant REGEX for item properties

---
+++ practice/src/javascripts/constants/regex.js
@@ -0,0 +1,7 @@
+const REGEX_PATTERNS = {
+  digitRegex: /^-?\d*\.?\d+$/,
+  integerRegex: /^-?\d+$/,
+  imageUrlRegex: /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg))/i,
+};
+
+export { REGEX_PATTERNS };
