645d17a469d85b3c75e6a41e81feb3e1e9a73323
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-05-30 15:46:21 +0700

    feat: add 'active' class to the parent nav-item of the clicked nav-link

--- practice/src/javascripts/helpers/sidebarHandler.js
+++ practice/src/javascripts/helpers/sidebarHandler.js
@@ -1,13 +1,11 @@
 function handleNavLinkClick(event) {
-  // Check if the clicked element is a nav-link inside a nav-item
-  if (event.target.classList.contains('nav-link')) {
-    const clickedNavItem = event.target.closest('.nav-item');
-
+  // Check if the clicked element or its parent is a nav-link inside a nav-item
+  const clickedNavItem = event.target.closest('.nav-item');
+  if (clickedNavItem) {
     // Check if the clicked nav-item is already active
     if (!clickedNavItem.classList.contains('active')) {
       // Remove 'active' class from the currently active nav-item
       const activeNavItem = document.querySelector('.nav-item.active');
-
       if (activeNavItem) {
         activeNavItem.classList.remove('active');
       }
