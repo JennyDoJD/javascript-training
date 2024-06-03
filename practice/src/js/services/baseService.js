f281ac397621d4946910df753cad1267c9c697be
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-03 14:41:14 +0700

    feat: add base fucntion to method delete

--- practice/src/js/services/baseService.js
+++ practice/src/js/services/baseService.js
@@ -12,4 +12,13 @@ export default class BaseService {
   async getAll() {
     return await APIHelper.get(this.endpoint);
   }
+
+  /**
+   * Delete a item by its ID
+   * @param {string} id - The ID of the item
+   * @param {Object} result - The result
+   */
+  async delete(id) {
+    return await APIHelper.delete(`${this.endpoint}/${id}`);
+  }
 }
