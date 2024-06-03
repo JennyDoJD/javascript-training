61cdfbe114cd25907c1daf272e9ff319c5657f08
JennyDoJD <loido.ued@gmail.com>
2024-06-04 06:53:15 +0700

    fix: binds the click event to handle product deletion

--- practice/src/js/products/productList.js
+++ practice/src/js/products/productList.js
@@ -4,6 +4,12 @@ export default class ProductList {
     this.productTemplate = template;
   }
 
+  async init() {
+    await this.renderProducts();
+
+    this.bindDeleteProductEvent();
+  }
+
   /**
    * Fetch all products from the ProductService
    * Render the products using the ProductTemplate component
@@ -12,5 +18,36 @@ export default class ProductList {
     const products = await this.productService.getAll();
 
     this.productTemplate.renderProducts(products);
+  }
+
+  /**
+   * Binds the click event to handle product deletion.
+   * When a delete icon is clicked, it displays a delete modal,
+   * deletes the corresponding product, and shows success/failure messages.
+   */
+  bindDeleteProductEvent() {
+    document.addEventListener('click', async (event) => {
+      const deleteIcon = event.target.closest('[data-product-id]');
+
+      if (deleteIcon) {
+        const productId = deleteIcon.dataset.productId;
+
+        try {
+          this.productTemplate.showDeleteModal(async () => {
+            const response = await this.productService.delete(productId);
+
+            if (response.isSuccess) {
+              this.productTemplate.showDeleteSuccessToast();
+              await this.renderProducts(); // await for the rendering after deletion
+            } else {
+              this.productTemplate.showDeleteFailureToast();
+            }
+          });
+        } catch (error) {
+          console.error(error);
+          this.productTemplate.showDeleteFailureToast();
+        }
+      }
+    });
   }
 }
