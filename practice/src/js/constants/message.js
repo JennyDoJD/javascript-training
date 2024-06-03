95d11a91741d150147b33b4da09fb5fa42c6181a
JennyDoJD <loido.ued@gmail.com>
2024-06-03 07:21:24 +0700

    fix: update content for messages

--- practice/src/js/constants/message.js
+++ practice/src/js/constants/message.js
@@ -1,10 +1,10 @@
 const MESSAGES = {
-  ADD_PRODUCT_SUCCESS_MSG: 'The dish was added successfully!',
-  ADD_PRODUCT_FAILED_MSG: 'The dish was added unsuccessfully!',
-  DELETE_PRODUCT_SUCCESS_MSG: 'The dish was deleted successfully!',
-  DELETE_PRODUCT_FAILED_MSG: 'The dish was deleted successfully!',
-  EDIT_PRODUCT_SUCCESS_MSG: 'The dish was edited successfully!',
-  EDIT_PRODUCT_SUCCESS_MSG: 'The dish was edited unsuccessfully!',
+  ADD_PRODUCT_SUCCESS_MSG: 'Succeed to add the product!',
+  ADD_PRODUCT_FAILED_MSG: 'Failed to add the product!',
+  DELETE_PRODUCT_SUCCESS_MSG: 'Succeed to delete the product!',
+  DELETE_PRODUCT_FAILED_MSG: 'Failed to delete the product!',
+  EDIT_PRODUCT_SUCCESS_MSG: 'Succeed to edit the product!',
+  EDIT_PRODUCT_FAILED_MSG: 'Failed to edit the product!',
   SEARCH_PRODUCT_LIST_EMPTY_HEADING: 'No dishes found!',
 };
 
