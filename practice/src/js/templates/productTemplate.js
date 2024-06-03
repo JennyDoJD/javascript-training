7f9adbc82e4fc97e45c108c293174e65a0922909
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-03 17:39:24 +0700

    fix: remove redundant comment

--- practice/src/js/templates/productTemplate.js
+++ practice/src/js/templates/productTemplate.js
@@ -80,7 +80,6 @@ export default class ProductTemplate {
 
   /**
    * Renders a form for creating a product
-   * @param {Object} [data={}] - The product data object containing id, name, price, imageURL, and quantity. Defaults to an empty object for creating a new product.
    */
   renderProductFormPage(data = {}) {
     this.clearMainContainer();
