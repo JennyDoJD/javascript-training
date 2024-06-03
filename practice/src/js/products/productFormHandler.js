93721f221ef5f062e4de12d3dd6684407e552163
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-03 14:59:16 +0700

    fix: rename function

--- practice/src/js/products/productFormHandler.js
+++ practice/src/js/products/productFormHandler.js
@@ -1,11 +1,17 @@
 import ProductService from '../services/productService';
-import { validateForm } from '../helpers/validateForm';
+import { validateProductForm } from '../helpers/validateForm';
 
 export default class ProductFormHandler {
   constructor() {
     this.productService = new ProductService();
   }
 
+  /**
+   * Initializes the product form by adding a submit event listener.
+   * When the form is submitted, it prevents the default form submission behavior,
+   * retrieves input values, validates the form, and adds the product using ProductService.
+   * If there are any validation errors, they are handled appropriately.
+   */
   init() {
     const productForm = document.getElementById('product-form');
 
@@ -22,7 +28,7 @@ export default class ProductFormHandler {
       const imageURL = imageURLInput.value;
       const quantity = parseInt(quantityInput.value);
 
-      const validationResult = validateForm({
+      const validationResult = validateProductForm({
         Name: name,
         Price: price,
         ImageURL: imageURL,
