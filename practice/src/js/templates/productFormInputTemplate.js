d0016768faf53fcf903db6ad8a024db9bbc14291
JennyDoJD <loido.ued@gmail.com>
2024-06-05 06:37:28 +0700

    fix: add value for formInputs

--- practice/src/js/templates/productFormInputTemplate.js
+++ practice/src/js/templates/productFormInputTemplate.js
@@ -7,12 +7,12 @@ const renderFormInputTemplate = (formInputs) => {
   let formInputsHTML = '';
 
   for (const item of formInputs) {
-    const { field, id } = item;
+    const { field, id, value = '' } = item;
 
     formInputsHTML += `
       <div class="form-group">
         <label for="${id}" class="tertiary-title">${field}</label>
-        <input class="input-form input-form-lg" id="${id}" type="text" />
+        <input class="input-form input-form-lg" id="${id}" type="text" value="${value}"/>
         <div class="error-message" id="${id}-error"></div>
       </div>
     `;
