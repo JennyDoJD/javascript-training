a99c5d13b08fd1eeeb6c095fc12b3ccbd780e32a
JennyDoJD <loido.ued@gmail.com>
2024-06-07 21:48:14 +0700

    fix: add message when products are not displayed

--- practice/src/js/constants/message.js
+++ practice/src/js/constants/message.js
@@ -1,6 +1,7 @@
 const MESSAGES = {
   DELETE_PRODUCT_SUCCESS_MSG: 'Succeed to delete the product!',
   DELETE_PRODUCT_FAILED_MSG: 'Failed to delete the product!',
+  NOT_FOUND_MSG: 'Not found!',
 };
 
 export { MESSAGES };
