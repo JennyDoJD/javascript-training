9cb4e1dfe085cc7bd1e21b4eb6c458dd64d71b31
JennyDoJD <loido.ued@gmail.com>
2024-06-10 07:13:05 +0700

    fix: add sortProducts method to BaseService class

--- practice/src/js/services/productService.js
+++ practice/src/js/services/productService.js
@@ -5,4 +5,8 @@ export default class ProductService extends BaseService {
   constructor() {
     super(API.PRODUCTS_ENDPOINT);
   }
+
+  async sortProducts(field, orderBy = '') {
+    return await this.sort(field, orderBy);
+  }
 }
