3b7d96d0861f048b954dac162e0192b6f436a1d3
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-10 16:50:12 +0700

    fix: remove redundant code

--- practice/src/js/services/productService.js
+++ practice/src/js/services/productService.js
@@ -5,8 +5,4 @@ export default class ProductService extends BaseService {
   constructor() {
     super(API.PRODUCTS_ENDPOINT);
   }
-
-  async sortProducts(field, orderBy = '') {
-    return await this.sort(field, orderBy);
-  }
 }
