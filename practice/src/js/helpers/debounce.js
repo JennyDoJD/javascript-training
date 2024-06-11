8cf056b216aa409d7dca694355df5f6c495ae9df
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-11 13:22:39 +0700

    fix: create debounce fucntion

---
+++ practice/src/js/helpers/debounce.js
@@ -0,0 +1,19 @@
+/**
+ * Debounces a function to limit its execution frequency.
+ * @param {Function} func - The function to debounce.
+ * @param {number} delay - The delay in milliseconds.
+ * @returns {Function} - The debounced function.
+ */
+
+function debounce(func, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+export { debounce };
