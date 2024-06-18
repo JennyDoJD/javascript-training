ed9386f69b23f2387d884b1119e2893519e72b2c
JennyDoJD <loido.ued@gmail.com>
2024-06-19 06:40:10 +0700

    fix: add event listener to the nav-list

--- practice/src/js/helpers/sidebar.js
+++ practice/src/js/helpers/sidebar.js
@@ -1,19 +1,26 @@
+/**
+ * Handles the click event on navigation links.
+ * @param {Event} event - The click event triggered by the user.
+ */
 function handleNavLinkClick(event) {
-  // Check if the clicked element or its parent is a nav-link inside a nav-item
   const navLinkItem = event.target.closest('.nav-item');
+
   if (navLinkItem) {
-    // Check if the clicked nav-item is already active
     if (!navLinkItem.classList.contains('active')) {
-      // Remove 'active' class from the currently active nav-item
       const activeNavItem = document.querySelector('.nav-item.active');
+
       if (activeNavItem) {
         activeNavItem.classList.remove('active');
       }
 
-      // Add 'active' class to the parent nav-item of the clicked nav-link
       navLinkItem.classList.add('active');
     }
   }
 }
 
-export { handleNavLinkClick };
+export default handleNavLinkClick;
+
+// Add event listener to the nav-list to handle clicks on navigation links
+document
+  .querySelector('.nav-list')
+  .addEventListener('click', handleNavLinkClick);
