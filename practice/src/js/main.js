da5347615e6eb16817be299d3f8079394ef863f1
JennyDoJD <loido.ued@gmail.com>
2024-06-19 06:39:09 +0700

    fix: change path import of handleNavLinkClick

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -1,8 +1,8 @@
 import ProductList from './products/productList';
 import ProductTemplate from './templates/productTemplate';
 import ProductService from './services/productService';
-import { handleNavLinkClick } from './helpers/sidebarHandler';
 import ProductForm from './products/productForm';
+import handleNavLinkClick from './helpers/sidebar';
 
 // Render products using ProductList instance
 const service = new ProductService();
@@ -10,11 +10,6 @@ const template = new ProductTemplate();
 const productList = new ProductList(service, template);
 
 productList.init();
-
-// Add event listener to the nav-list to handle clicks on navigation links
-document
-  .querySelector('.nav-list')
-  .addEventListener('click', handleNavLinkClick);
 
 // Initialize the handler for the form
 const productForm = new ProductForm(service, template);
