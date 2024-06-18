477de7a260ef219dfd3dcb3319bc923baac22a46
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-18 11:35:37 +0700

    fix: add image format

--- practice/src/js/constants/regex.js
+++ practice/src/js/constants/regex.js
@@ -1,7 +1,7 @@
 const REGEX_PATTERNS = {
   digitRegex: /^-?\d*\.?\d+$/,
   integerRegex: /^-?\d+$/,
-  imageUrlRegex: /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg))/i,
+  imageUrlRegex: /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg|webp))/i,
 };
 
 export { REGEX_PATTERNS };
