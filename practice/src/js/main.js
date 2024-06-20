4aa362d0aff3f6d583bf1a1c9a2aecc835e05fd3
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-20 15:27:52 +0700

    fix: add import action constants

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -1,9 +1,10 @@
 import ProductList from './products/productList';
 import ProductForm from './products/productForm';
+import ProductForm from './products/productForm';
 import ProductTemplate from './templates/productTemplate';
 import ProductService from './services/productService';
 import handleNavLinkClick from './helpers/sidebarHandler';
-import ProductForm from './products/productForm';
+import action from './helpers/actionHandler';
 
 // Render products using ProductList instance
 const service = new ProductService();
