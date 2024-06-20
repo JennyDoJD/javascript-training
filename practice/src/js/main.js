a8d0679a18b981e423889e2caed0306b69f57dad
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-20 09:03:04 +0700

    fix: move related import closer together

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -1,9 +1,9 @@
 import ProductList from './products/productList';
+import ProductForm from './products/productForm';
 import ProductTemplate from './templates/productTemplate';
 import ProductService from './services/productService';
-import { handleNavLinkClick } from './helpers/sidebarHandler';
-import ProductForm from './products/productForm';
 import action from './helpers/action';
+import { handleNavLinkClick } from './helpers/sidebarHandler';
 
 // Render products using ProductList instance
 const service = new ProductService();
