1f3bdd3e3b3ff71d7deeece434c8931697fe2897
JennyDoJD <loido.ued@gmail.com>
2024-06-21 05:46:40 +0700

    fix: initialize components and event listeners using IIFE

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -1,24 +1,25 @@
 import ProductList from './products/productList';
 import ProductForm from './products/productForm';
-import ProductForm from './products/productForm';
 import ProductTemplate from './templates/productTemplate';
 import ProductService from './services/productService';
 import handleNavLinkClick from './helpers/sidebarHandler';
 import action from './helpers/actionHandler';
 
-// Render products using ProductList instance
-const service = new ProductService();
-const template = new ProductTemplate();
-const productList = new ProductList(service, template);
+(function () {
+  // Initialize the ProductList
+  const service = new ProductService();
+  const template = new ProductTemplate();
+  const productList = new ProductList(service, template);
 
-productList.init();
+  productList.init();
 
-// Add event listener to the nav-list to handle clicks on navigation links
-document
-  .querySelector('.nav-list')
-  .addEventListener('click', handleNavLinkClick);
+  // Initialize the ProductForm with appropriate action (add or edit)
+  const productForm = new ProductForm(service, template, action);
 
-// Initialize the handler for the form
-const productForm = new ProductForm(service, template, action);
+  productForm.init();
 
-productForm.init();
+  // Add event listener to the nav-list to handle clicks on navigation links
+  document
+    .querySelector('.nav-list')
+    .addEventListener('click', handleNavLinkClick);
+})();
