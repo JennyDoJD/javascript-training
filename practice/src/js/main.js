12f360d536194d4f9004938d658b81aa5790a9f6
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-25 17:51:27 +0700

    fix: remove init for productForm

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -1,9 +1,7 @@
 import ProductList from './products/productList';
-import ProductForm from './products/productForm';
 import ProductTemplate from './templates/productTemplate';
 import ProductService from './services/productService';
 import handleNavLinkClick from './helpers/sidebarHandler';
-import action from './helpers/actionHandler';
 
 (function () {
   // Initialize the ProductList
@@ -12,14 +10,4 @@ import action from './helpers/actionHandler';
   const productList = new ProductList(service, template);
 
   productList.init();
-
-  // Initialize the ProductForm with appropriate action (add or edit)
-  const productForm = new ProductForm(service, template, action);
-
-  productForm.init();
-
-  // Add event listener to the nav-list to handle clicks on navigation links
-  document
-    .querySelector('.nav-list')
-    .addEventListener('click', handleNavLinkClick);
 })();
