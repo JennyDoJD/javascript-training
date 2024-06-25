315ed1635994022af498bcc146c71fe7f2944585
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-25 10:29:19 +0700

    fix: remove redundant code

--- practice/src/js/templates/productTemplate.js
+++ practice/src/js/templates/productTemplate.js
@@ -175,20 +175,6 @@ export default class ProductTemplate {
   };
 
   /**
-   * Display a toast notification for successful deletion.
-   */
-  showDeleteSuccessToast() {
-    Toast.success(MESSAGES.DELETE_PRODUCT_SUCCESS_MSG);
-  }
-
-  /**
-   * Display a toast notification for deletion failure.
-   */
-  showDeleteFailureToast() {
-    Toast.error(MESSAGES.DELETE_PRODUCT_FAILED_MSG);
-  }
-
-  /**
    * Binds the search input to the search handler function and render the results.
    * @param {Function} handlerSearchProduct - The handler function to fetch and return search results.
    */
