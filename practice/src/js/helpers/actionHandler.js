8347202ec189cd2ec4bce3856ba9f0fb1806ed21
JennyDoJD <loido.ued@gmail.com>
2024-06-28 06:13:43 +0700

    fix: add comment for getActionFromUrl()

--- practice/src/js/helpers/actionHandler.js
+++ practice/src/js/helpers/actionHandler.js
@@ -1,5 +1,9 @@
 import { ACTION } from '../constants/actionType';
 
+/**
+ * Determines the action to be taken based on the URL parameters.
+ * @returns {string} - Returns the appropriate action type, either ACTION.EDIT or ACTION.ADD.
+ */
 function getActionFromUrl() {
   const urlParams = new URLSearchParams(window.location.search);
   const productId = urlParams.get('id');
