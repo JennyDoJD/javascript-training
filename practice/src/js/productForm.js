451838b051fd71d9ee1854cc8eb0a08f38029884
JennyDoJD <loido.ued@gmail.com>
2024-06-28 06:11:07 +0700

    fix: init ProductForm with action derived from URL

--- practice/src/js/productForm.js
+++ practice/src/js/productForm.js
@@ -1,8 +1,10 @@
 import ProductForm from './products/productForm';
 import handleNavLinkClick from './helpers/sidebarHandler';
-import action from './helpers/actionHandler';
+import getActionFromUrl from './helpers/actionHandler';
 
 (function () {
+  const action = getActionFromUrl();
+
   // Initialize the ProductForm with appropriate action (add or edit)
   new ProductForm(action);
 })();
