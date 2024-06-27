d574c6a70e2187846aaf103d112421723e1b462b
JennyDoJD <loido.ued@gmail.com>
2024-06-28 06:08:24 +0700

    fix: refactor code

--- practice/src/js/helpers/actionHandler.js
+++ practice/src/js/helpers/actionHandler.js
@@ -1,19 +1,16 @@
 import { ACTION } from '../constants/actionType';
 
 function getActionFromUrl() {
-  let action;
   const urlParams = new URLSearchParams(window.location.search);
   const productId = urlParams.get('id');
 
   if (productId) {
-    action = ACTION.EDIT;
     document.title = 'Edit Product';
+    return ACTION.EDIT;
   } else {
-    action = ACTION.ADD;
     document.title = 'Create a new product';
+    return ACTION.ADD;
   }
-
-  return action;
 }
 
 export default getActionFromUrl;
