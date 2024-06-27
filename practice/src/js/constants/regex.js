8b118e516ce97db3f65b7edd2b8963796c607d19
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-27 10:54:36 +0700

    fix: add alphaNumericCharacterRegex to REGEX_PATTERNS

--- practice/src/js/constants/regex.js
+++ practice/src/js/constants/regex.js
@@ -2,6 +2,8 @@ const REGEX_PATTERNS = {
   digitRegex: /^-?\d*\.?\d+$/,
   integerRegex: /^-?\d+$/,
   imageUrlRegex: /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg|webp))/i,
+  alphaNumericCharacterRegex:
+    /^(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*(),.?":{}|<>~`_+=-]*$/,
 };
 
 export { REGEX_PATTERNS };
