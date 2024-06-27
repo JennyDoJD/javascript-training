eeb28c523731935e63946aeb8498eeecb83ea060
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-06-27 11:16:00 +0700

    fix: update alphaNumericCharacterRegex to accept spaces

--- practice/src/js/constants/regex.js
+++ practice/src/js/constants/regex.js
@@ -3,7 +3,7 @@ const REGEX_PATTERNS = {
   integerRegex: /^-?\d+$/,
   imageUrlRegex: /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg|webp))/i,
   alphaNumericCharacterRegex:
-    /^(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*(),.?":{}|<>~`_+=-]*$/,
+    /^(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*(),.?":{}|<>~`_+=-\s]*$/,
 };
 
 export { REGEX_PATTERNS };
