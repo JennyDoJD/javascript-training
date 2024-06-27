0349ce793443f50bf2326de370b4b6ae45a4a82b
JennyDoJD <loido.ued@gmail.com>
2024-06-28 06:28:08 +0700

    fix: add event listener to handle clicks on nav links for productForm.js

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -4,10 +4,14 @@ import ProductService from './services/productService';
 import handleNavLinkClick from './helpers/sidebarHandler';
 
 (function () {
-  // Initialize the ProductList
   const service = new ProductService();
   const template = new ProductTemplate();
   const productList = new ProductList(service, template);
+  const navList = document.querySelector('.nav-list');
 
+  // Initialize the ProductList
   productList.init();
+
+  // Add event listener to the nav-list to handle clicks on navigation links
+  navList.addEventListener('click', handleNavLinkClick);
 })();
