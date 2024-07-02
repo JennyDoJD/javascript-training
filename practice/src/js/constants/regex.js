01a42d23ef8b6dc957836b740ea1377183d27fb8
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-02 13:03:34 +0700

    fix: refine imageUrlRegex to allow optional scheme

--- practice/src/js/constants/regex.js
+++ practice/src/js/constants/regex.js
@@ -1,7 +1,7 @@
 const REGEX_PATTERNS = {
   digitRegex: /^-?\d*\.?\d+$/,
   integerRegex: /^-?\d+$/,
-  imageUrlRegex: /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg|webp))/i,
+  imageUrlRegex: /^((http[s]?:)?\/\/.*\.(?:png|jpg|gif|svg|jpeg|webp))/i,
   alphaNumericCharacterRegex:
     /^(?=.*[a-zA-ZÀ-Ỹà-ỹ])[a-zA-ZÀ-Ỹà-ỹ0-9!@#$%^&*(),.?":{}|<>~`_+=\-\/\s]*$/,
 };
